59565bc7c867b7e745f0ffb0b6d66dfd38d1764e
adilet berdibekov <berdibekovadilet@gmail.com>
2022-10-29 23:01:29 +0600

    added post service and controller

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,11 +1,24 @@
-import { Injectable } from '@nestjs/common';
-import { CreatePostDto } from './dto/create-post.dto';
-import { UpdatePostDto } from './dto/update-post.dto';
+import { Injectable } from "@nestjs/common";
+import { InjectModel } from "@nestjs/sequelize";
+import { FilesService } from "src/files/files.service";
+import { CreatePostDto } from "./dto/create-post.dto";
+import { UpdatePostDto } from "./dto/update-post.dto";
+import { Post } from "./posts.model";
 
 @Injectable()
 export class PostsService {
-  create(createPostDto: CreatePostDto) {
-    return 'This action adds a new post';
+  constructor(
+    @InjectModel(Post) private postRepository: typeof Post,
+    private fileService: FilesService
+  ) {}
+
+  async create(createPostDto: CreatePostDto, image: any) {
+    const fileName = await this.fileService.createFile(image);
+    const post = await this.postRepository.create({
+      ...createPostDto,
+      image: fileName,
+    });
+    return post;
   }
 
   findAll() {
