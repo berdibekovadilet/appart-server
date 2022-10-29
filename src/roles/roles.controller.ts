e34bd738474935f09f5d289bcdf8a16cb12e644c
adilet berdibekov <berdibekovadilet@gmail.com>
2022-10-29 22:54:03 +0600

    added roles service and controller

--- src/roles/roles.controller.ts
+++ src/roles/roles.controller.ts
@@ -1,7 +1,25 @@
-import { Controller } from '@nestjs/common';
-import { RolesService } from './roles.service';
+import { Controller, Get, Post, Body, Param } from "@nestjs/common";
+import { RolesService } from "./roles.service";
+import { CreateRoleDto } from "./dto/create-role.dto";
+import { ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
+import { Role } from "./roles.model";
 
-@Controller('roles')
+@ApiTags("Роли")
+@Controller("roles")
 export class RolesController {
-  constructor(private readonly rolesService: RolesService) {}
+  constructor(private readonly roleService: RolesService) {}
+
+  @ApiOperation({ summary: "Создание роли" })
+  @ApiResponse({ status: 200, type: Role })
+  @Post()
+  create(@Body() dto: CreateRoleDto) {
+    return this.roleService.createRole(dto);
+  }
+
+  @ApiOperation({ summary: "Получить роль пользователя" })
+  @ApiResponse({ status: 200, type: Role })
+  @Get("/:value")
+  getByValue(@Param("value") value: string) {
+    return this.roleService.getRoleByValue(value);
+  }
 }
