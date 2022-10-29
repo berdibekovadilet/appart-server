862ebd8c91078daf70d60c8d1e61c4d6bead0bf5
adilet berdibekov <berdibekovadilet@gmail.com>
2022-10-29 22:52:03 +0600

    added users service and controller

--- src/roles/roles.service.ts
+++ src/roles/roles.service.ts
@@ -1,4 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
+import { InjectModel } from "@nestjs/sequelize";
+import { CreateRoleDto } from "./dto/create-role.dto";
+import { Role } from "./roles.model";
 
 @Injectable()
-export class RolesService {}
+export class RolesService {
+  constructor(@InjectModel(Role) private roleRepository: typeof Role) {}
+
+  async createRole(createRoleDto: CreateRoleDto) {
+    const role = await this.roleRepository.create(createRoleDto);
+    return role;
+  }
+
+  async getRoleByValue(value: string) {
+    const role = await this.roleRepository.findOne({ where: { value } });
+    return role;
+  }
+}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,34 +1,81 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { UsersService } from './users.service';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+  UsePipes,
+} from "@nestjs/common";
+import { UsersService } from "./users.service";
+import { CreateUserDto } from "./dto/create-user.dto";
+import { UpdateUserDto } from "./dto/update-user.dto";
+import { ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
+import { User } from "./users.model";
+import { Roles } from "src/auth/roles-auth.decorator";
+import { RolesGuard } from "src/auth/roles.guard";
+import { AddRoleDto } from "./dto/add-role.dto";
+import { BanUserDto } from "./dto/ban-user.dto";
 
-@Controller('users')
+@ApiTags("Пользователи")
+@Controller("users")
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @ApiOperation({ summary: "Создание пользователя" })
+  @ApiResponse({ status: 200, type: User })
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
-    return this.usersService.create(createUserDto);
+    return this.usersService.createUser(createUserDto);
   }
 
+  @ApiOperation({ summary: "Получить всех пользователей" })
+  @ApiResponse({ status: 200, type: [User] })
+  @Roles("ADMIN")
+  @UseGuards(RolesGuard)
   @Get()
   findAll() {
-    return this.usersService.findAll();
+    return this.usersService.getAllUser();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
+  @ApiOperation({ summary: "Выдать роль" })
+  @ApiResponse({ status: 200 })
+  @Roles("ADMIN")
+  @UseGuards(RolesGuard)
+  @Post("/role")
+  addRole(@Body() dto: AddRoleDto) {
+    return this.usersService.addRole(dto);
+  }
+
+  @ApiOperation({ summary: "Забанить пользователя" })
+  @ApiResponse({ status: 200 })
+  @Roles("ADMIN")
+  @UseGuards(RolesGuard)
+  @Post("/ban")
+  ban(@Body() dto: BanUserDto) {
+    return this.usersService.ban(dto);
+  }
+
+  @ApiOperation({ summary: "Получить пользователя" })
+  @ApiResponse({ status: 200, type: User })
+  @Get(":id")
+  findOne(@Param("id") id: string) {
     return this.usersService.findOne(+id);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+  @ApiOperation({ summary: "Обновить пользователя" })
+  @ApiResponse({ status: 200, type: User })
+  @Patch(":id")
+  update(@Param("id") id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.update(+id, updateUserDto);
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
+  @ApiOperation({ summary: "Удалить пользователя" })
+  @ApiResponse({ status: 200, type: User })
+  @Delete(":id")
+  remove(@Param("id") id: string) {
     return this.usersService.remove(+id);
   }
 }
